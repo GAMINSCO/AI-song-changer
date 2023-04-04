c84736fd223bc29c9c0c98e6d7a0630d121b76a0
GAMINSCO <83284620+GAMINSCO@users.noreply.github.com>
2023-04-04 12:39:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-function preload() {}
+song1 = "";
+song2 = "";
+
+function preload() {
+song1 = loadSound("music1.mp3");
+song2 = loadSound("music2.mp3");
+}
 
 function setup() {
    Canvas = createCanvas(500, 400);
    Canvas.position(440, 220);
+
+   video = createCapture(VIDEO);
+   video.hide()
+}
+
+function draw() {
+   image(video, 0, 0, 500, 400);
 }
